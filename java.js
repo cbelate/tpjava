93004c020f51d907f4430eab907aac0dd6272933
cbelate <145708961+cbelate@users.noreply.github.com>
2023-10-12 13:40:20 -0300

    Add files via upload

---
+++ java.js
@@ -0,0 +1,83 @@
+
+//Realice un algoritmo que muestre el nombre y apellido de un usuario//
+var nombre = prompt (
+    "Ingrese su Nombre de Usuario"
+)
+var apellido = prompt (
+    "Ingrese su Apellido"
+)
+alert ( nombre + " " + apellido)
+
+
+// Realice un algoritmo que muestre el promedio de 3 notas//
+
+var n1 = prompt (
+    "Ingrese primer numero "
+)
+n1=Number (n1)
+
+var n2 = prompt (
+    "Ingrese segundo numero "
+)
+n2=Number (n2)
+
+var n3 = prompt (
+    "ingrese tercer numero "
+)
+n3=Number (n3)
+
+alert ( (n1+n2+n3) /3 )
+
+// Realice el algoritmo para calcular e informar el perímetro del triángulo //
+
+var lt1 = prompt (
+    "Ingrese medida primer lado"
+)
+lt1 = Number(lt1)
+
+var lt2 = prompt (
+    "Ingrese medida segundo lado"
+)
+lt2 = Number(lt2)
+
+var lt3 = prompt (
+    "Ingrese medida tercer lado"
+)
+lt3 = Number(lt3)
+
+alert (lt1+lt2+lt3)
+
+//Realice el algoritmo para calcular e informar el perímetro y el área del cuadrado.//
+
+var lc1 = prompt (
+    "Ingrese medida primer lado"
+)
+lc1 = Number(lc1)
+
+var lc2 = prompt (
+    "Ingrese medida segundo lado"
+)
+lc2 = Number(lc2)
+
+var lc3 = prompt (
+    "Ingrese medida tercer lado"
+)
+lc3 = Number(lc3)
+
+var lc4 = prompt (
+    "Ingrese medida cuarto lado"
+)
+lc4 = Number(lc4)
+
+var Perimetro = (lc1+lc2+lc3+lc4)
+
+Perimetro =Number (Perimetro)
+
+alert ("El perimetro del cuadrado es:"+Perimetro)
+
+var superficie = (lc1*lc2*lc3*lc4)
+
+superficie=Number (superficie)
+
+alert ("La superficie del cuadrado es:"+superficie)
+
