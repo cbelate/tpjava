1ee7f23bcd5cb784ab87ac3eea84c755d4696e5d
mari1551 <141951805+mari1551@users.noreply.github.com>
2023-10-12 15:27:04 -0300

    agregando ejercicios del 6  al 8 unidad I

--- java.js
+++ java.js
@@ -1,5 +1,5 @@
-
-//Realice un algoritmo que muestre el nombre y apellido de un usuario//
+//Unidad I
+//Ej1- Realice un algoritmo que muestre el nombre y apellido de un usuario//
 var nombre = prompt (
     "Ingrese su Nombre de Usuario"
 )
@@ -9,7 +9,7 @@ var apellido = prompt (
 alert ( nombre + " " + apellido)
 
 
-// Realice un algoritmo que muestre el promedio de 3 notas//
+//Ej2- Realice un algoritmo que muestre el promedio de 3 notas//
 
 var n1 = prompt (
     "Ingrese primer numero "
@@ -28,7 +28,7 @@ n3=Number (n3)
 
 alert ( (n1+n2+n3) /3 )
 
-// Realice el algoritmo para calcular e informar el perímetro del triángulo //
+// Ej3 - Realice el algoritmo para calcular e informar el perímetro del triángulo //
 
 var lt1 = prompt (
     "Ingrese medida primer lado"
@@ -47,7 +47,7 @@ lt3 = Number(lt3)
 
 alert (lt1+lt2+lt3)
 
-//Realice el algoritmo para calcular e informar el perímetro y el área del cuadrado.//
+//Ej4 - Realice el algoritmo para calcular e informar el perímetro y el área del cuadrado.//
 
 var lc1 = prompt (
     "Ingrese medida primer lado"
@@ -71,7 +71,7 @@ superficie=Number (superficie)
 
 alert ("La superficie del cuadrado es:"+superficie)
 
-//Realice el algoritmo para calcular e informar el perímetro y el área del rectángulo//
+//Ej5 - Realice el algoritmo para calcular e informar el perímetro y el área del rectángulo//
 
 var br = prompt (
     "Ingrese medida de la base del rectangulo"
@@ -95,4 +95,25 @@ superficierec=Number (superficierec)
 
 alert ("La superficie del cuadrado es:"+superficierec)
 
-//Realice el algoritmo para calcular e informar el resto de la división entera entre los valores.//
+//Ej6- Realice el algoritmo para calcular e informar el resto de la división entera entre los valores.//
+var numero1=50;
+var numero2=2;
+var resto=numero1%numero2;
+alert("El resto de la division "+numero1+"/"+numero2+" es: "+resto);
+
+//Ej7- Realice el algoritmo que arme y muestre el mensaje "Bienvenido", seguido del nombre y apellido del usuario.
+var usuario='Mariana1551';
+alert("Bienvenido "+usuario);
+
+//Ej8 - Realice el algoritmo para calcular e informar el perímetro y el área del círculo. Se considerará para este ejercicio que el valor de PI será 3.1416.
+const PI=3.1416;
+var radio=5;
+var perimetro = 2* PI * radio;
+var area = PI * radio * radio;
+alert("Con circulo de radio: "+radio+"\n"+"El perimetro del circulo es: "+perimetro+"\n"+"El area del circulo es: "+area);
+
+
+
+
+
+
